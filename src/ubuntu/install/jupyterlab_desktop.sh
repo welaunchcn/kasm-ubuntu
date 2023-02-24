f9c3427d193aa66f771c0c04bb0f7e7d8095fa54
welaunch <welaunch@welaunch.cn>
2023-02-25 00:49:04 +0800

    jupyterlab settings

--- src/ubuntu/install/jupyterlab_desktop.sh
+++ src/ubuntu/install/jupyterlab_desktop.sh
@@ -7,3 +7,5 @@ dpkg -i JupyterLab-Setup-Debian.deb
 rm JupyterLab-Setup-Debian.deb
 sed -i 's#/opt/JupyterLab/jupyterlab-desktop#/opt/JupyterLab/jupyterlab-desktop --no-sandbox --disable-setuid-sandbox#' /usr/share/applications/jupyterlab-desktop.desktop
 cp /usr/share/applications/jupyterlab-desktop.desktop $HOME/Desktop
+chmod +x $HOME/Desktop
+cp /dockerstartup/install/jupyterlab_desktop_settings.json $HOME/.config/jupyterlab-desktop/settings.json
